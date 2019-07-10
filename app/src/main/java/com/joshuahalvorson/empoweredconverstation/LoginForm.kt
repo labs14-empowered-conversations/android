7fdcf79bf021b061bbd75e4f8218411a61c1247e
VivekV95 <vivekv210@gmail.com>
2019-07-10 13:38:08 -0500

    Add required app dependencies

--- app/src/main/java/com/joshuahalvorson/empoweredconverstation/LoginForm.kt
+++ app/src/main/java/com/joshuahalvorson/empoweredconverstation/LoginForm.kt
@@ -3,12 +3,10 @@ package com.joshuahalvorson.empoweredconverstation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginForm : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        
+        setContentView(R.layout.activity_login_form)
     }
 }
