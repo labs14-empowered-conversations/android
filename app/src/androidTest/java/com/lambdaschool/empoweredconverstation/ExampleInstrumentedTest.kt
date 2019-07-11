2e8fa5501cf0fbab7b17ea650079c5d75fded27f
JoshHalvorson <joshhalvorson16@gmail.com>
2019-07-10 19:14:32 -0700

    Rename test package

--- app/src/androidTest/java/com/lambdaschool/empoweredconverstation/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/lambdaschool/empoweredconverstation/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.joshuahalvorson.empoweredconverstation
+package com.lambdaschool.empoweredconverstation
 
 import androidx.test.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
