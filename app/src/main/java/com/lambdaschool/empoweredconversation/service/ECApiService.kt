bdf1b9d3c713fa453174f4330da0ab1a4509fac9
VivekV95 <vivekv210@gmail.com>
2019-07-11 14:58:34 -0500

    Write interface function for access token

--- app/src/main/java/com/lambdaschool/empoweredconversation/service/ECApiService.kt
+++ app/src/main/java/com/lambdaschool/empoweredconversation/service/ECApiService.kt
@@ -1,5 +1,19 @@
 package com.lambdaschool.empoweredconversation.service
 
+import com.lambdaschool.empoweredconversation.Token
+import io.reactivex.Single
+import retrofit2.http.*
+
 interface ECApiService {
+
+    @FormUrlEncoded
+    @POST("/oauth/token")
+    @Headers("Content-Type: application/x-www-form-urlencoded")
+    fun getToken(
+        @Field("grant_type") type: String,
+        @Field("username") username: String,
+        @Field("password") password: String,
+        @Header("Authorization") header: String
+    ): Single<Token>
 
 }
