dcb914fe1bc147ba4c1708b2906d4421a5489432
JoshHalvorson <joshhalvorson16@gmail.com>
2019-07-24 15:01:04 -0700

    Remove video from landing page
    Still deciding where video is going to be in app.
    Co-authored-by: JoshHalvorson <joshhalvorson16@gmail.com>
    Co-authored-by: VivekV95 <vivekv210@gmail.com>

--- app/src/main/java/com/lambdaschool/empoweredconversation/activity/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/empoweredconversation/activity/MainActivity.kt
@@ -1,33 +1,13 @@
 package com.lambdaschool.empoweredconversation.activity
 
-import android.content.Intent
-import android.content.pm.ActivityInfo
-import android.content.res.Configuration
-import android.hardware.SensorManager
 import android.os.Bundle
-import android.view.OrientationEventListener
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import com.lambdaschool.empoweredconversation.R
 import com.lambdaschool.empoweredconversation.vm.LandingViewModel
-import com.mikepenz.materialdrawer.AccountHeaderBuilder
-import com.mikepenz.materialdrawer.Drawer
-import com.mikepenz.materialdrawer.DrawerBuilder
-import com.mikepenz.materialdrawer.model.DividerDrawerItem
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
-import com.mikepenz.materialdrawer.model.ProfileDrawerItem
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerCallback
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerFullScreenListener
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.ui.PlayerUiController
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var uiController: PlayerUiController
     private lateinit var landingViewModel: LandingViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,65 +17,6 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.title = "Home"
 
         landingViewModel = ViewModelProviders.of(this).get(LandingViewModel::class.java)
-
-        val youtubePlayerView = findViewById<YouTubePlayerView>(R.id.youtube_player_view)
-        lifecycle.addObserver(youtubePlayerView)
-        uiController = youtubePlayerView.getPlayerUiController()
-
-        val orientationEventListener = object : OrientationEventListener(applicationContext, SensorManager.SENSOR_DELAY_UI) {
-            override fun onOrientationChanged(orientation: Int) {
-                if (orientation <= 10) {
-                    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
-                }
-            }
-        }
-
-        if (orientationEventListener.canDetectOrientation()) {
-            orientationEventListener.enable()
-        }
-
-        initializeYoutubePlayer(youtube_player_view)
     }
 
-    private fun initializeYoutubePlayer(youtubePlayer: YouTubePlayerView) {
-        youtubePlayer.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
-            override fun onReady(youTubePlayer: YouTubePlayer) {
-                youTubePlayer.loadVideo("S0Q4gqBUs7c", 0f)
-            }
-        })
-
-        youtube_player_view.addFullScreenListener(object: YouTubePlayerFullScreenListener{
-            override fun onYouTubePlayerEnterFullScreen() {
-                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-                linear_layout_top.visibility = View.GONE
-                toolbar_card_view.visibility = View.GONE
-                hideSystemUI()
-            }
-
-            override fun onYouTubePlayerExitFullScreen() {
-                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-                linear_layout_top.visibility = View.VISIBLE
-                toolbar_card_view.visibility = View.VISIBLE
-                window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_VISIBLE)
-            }
-        })
-    }
-
-    override fun onBackPressed() {
-        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-
-    private fun hideSystemUI() {
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
-    }
 }
