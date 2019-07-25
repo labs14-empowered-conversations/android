0c4f14dfd4852f7cdd009a23f42523959494e4db
VivekV95 <vivekv210@gmail.com>
2019-07-25 15:28:29 -0500

    Modify Phrases class
    Co-authored-by: JoshHalvorson <joshhalvorson16@gmail.com>
    Co-authored-by: VivekV95 <vivekv210@gmail.com>

--- app/src/main/java/com/lambdaschool/empoweredconversation/PhraseList.kt
+++ app/src/main/java/com/lambdaschool/empoweredconversation/PhraseList.kt
@@ -3,18 +3,29 @@ package com.lambdaschool.empoweredconversation
 import kotlin.random.Random
 
 class PhraseList {
-    private val phrases: ArrayList<String> =
-        arrayListOf("Grab coffee or a drink",
-        "No drama Oxford comma",
-            "My height and shoulders",
-            "Life is short",
-            "Road trips pickles road trips",
-            "I have a crush on passionate about",
-            "Netflix Murakami",
-            "Having a few beers",
-            "Ethical nonmonogamy")
 
-    fun getPhrase(): String {
-        return phrases[Random.nextInt(0,9)]
+    companion object {
+        private var counter = 0
+
+        private val phrases: ArrayList<String> =
+            arrayListOf("Grab coffee or a drink",
+                "No drama Oxford comma",
+                "My height and shoulders",
+                "Life is short",
+                "Road trips pickles road trips",
+                "I have a crush on passionate about",
+                "Netflix Murakami",
+                "Having a few beers",
+                "Ethical nonmonogamy")
+
+        fun getNextPhrase(): String {
+            if (counter == 9) counter = 0
+            return phrases[counter++]
+        }
+
+        fun setStartingIndex() {
+            counter = Random.nextInt(0,8)
+        }
     }
+
 }
