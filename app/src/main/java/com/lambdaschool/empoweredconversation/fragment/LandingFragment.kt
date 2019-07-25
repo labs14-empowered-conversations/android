b91cb310351782bc460ce8bd850b50e43406db00
VivekV95 <vivekv210@gmail.com>
2019-07-25 15:28:56 -0500

    Update phrases carousel with cyclical phrases
    Co-authored-by: JoshHalvorson <joshhalvorson16@gmail.com>
    Co-authored-by: VivekV95 <vivekv210@gmail.com>

--- app/src/main/java/com/lambdaschool/empoweredconversation/fragment/LandingFragment.kt
+++ app/src/main/java/com/lambdaschool/empoweredconversation/fragment/LandingFragment.kt
@@ -1,32 +1,37 @@
 package com.lambdaschool.empoweredconversation.fragment
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.lambdaschool.empoweredconversation.PhraseList
 import com.lambdaschool.empoweredconversation.R
+import kotlinx.android.synthetic.main.fragment_landing.*
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class LandingFragment : Fragment() {
 
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_landing, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        updateCarousel()
+    }
+
+    private fun updateCarousel() {
+        PhraseList.setStartingIndex()
+        Thread(Runnable {
+            while (true) {
+                activity?.runOnUiThread { phrase_textview.text = PhraseList.getNextPhrase() }
+                Thread.sleep(5000)
+            }
+        }).start()
+
+    }
 
 }
