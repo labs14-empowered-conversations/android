a94331a779db79a089abdd8aad8eb38046600e9a
JoshHalvorson <joshhalvorson16@gmail.com>
2019-07-25 14:23:17 -0700

    Add animations for phrase carousel
    Co-authored-by: JoshHalvorson <joshhalvorson16@gmail.com>
    Co-authored-by: VivekV95 <vivekv210@gmail.com>

--- app/src/main/java/com/lambdaschool/empoweredconversation/fragment/LandingFragment.kt
+++ app/src/main/java/com/lambdaschool/empoweredconversation/fragment/LandingFragment.kt
@@ -1,16 +1,20 @@
 package com.lambdaschool.empoweredconversation.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
 import com.lambdaschool.empoweredconversation.PhraseList
 import com.lambdaschool.empoweredconversation.R
 import kotlinx.android.synthetic.main.fragment_landing.*
+import kotlinx.coroutines.*
 
 class LandingFragment : Fragment() {
-
+    private val carouselJob = Job()
+    private val carouselScope = CoroutineScope(Dispatchers.IO + carouselJob)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -25,13 +29,33 @@ class LandingFragment : Fragment() {
 
     private fun updateCarousel() {
         PhraseList.setStartingIndex()
-        Thread(Runnable {
-            while (true) {
-                activity?.runOnUiThread { phrase_textview.text = PhraseList.getNextPhrase() }
-                Thread.sleep(5000)
-            }
-        }).start()
+        carouselScope.launch {
+            while (phrase_textview != null) {
+                withContext(Dispatchers.Main) {
+                    phrase_textview.text = PhraseList.getNextPhrase()
+                    YoYo.with(Techniques.SlideInLeft)
+                        .duration(1000)
+                        .repeat(0)
+                        .playOn(phrase_textview)
+                }
 
+                Thread.sleep(3000)
+
+                withContext(Dispatchers.Main) {
+                    YoYo.with(Techniques.SlideOutRight)
+                        .duration(1000)
+                        .repeat(0)
+                        .playOn(phrase_textview)
+                }
+
+                Thread.sleep(1000)
+            }
+        }
     }
 
+    @ExperimentalCoroutinesApi
+    override fun onStop() {
+        super.onStop()
+        carouselJob.cancel()
+    }
 }
