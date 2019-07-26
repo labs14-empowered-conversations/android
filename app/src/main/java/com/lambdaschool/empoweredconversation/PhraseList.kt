8fdabdf4905974c71cee11e07c6d7807aa6bffd1
VivekV95 <vivekv210@gmail.com>
2019-07-25 22:35:23 -0500

    Change carousel phrases to Sagan Ipsum
    Co-authored-by: JoshHalvorson <joshhalvorson16@gmail.com>
    Co-authored-by: VivekV95 <vivekv210@gmail.com>

--- app/src/main/java/com/lambdaschool/empoweredconversation/PhraseList.kt
+++ app/src/main/java/com/lambdaschool/empoweredconversation/PhraseList.kt
@@ -8,15 +8,15 @@ class PhraseList {
         private var counter = 0
 
         private val phrases: ArrayList<String> =
-            arrayListOf("Grab coffee or a drink",
+            arrayListOf("From which we spring at the edge of forever",
                 "No drama Oxford comma",
-                "My height and shoulders",
-                "Life is short",
-                "Road trips pickles road trips",
-                "I have a crush on passionate about",
-                "Netflix Murakami",
-                "Having a few beers",
-                "Ethical nonmonogamy")
+                "Great turbulent clouds finite",
+                "Vastness is bearable only through love",
+                "The sky calls to us",
+                "The interiors of collapsing stars",
+                "Citizens of distant epochs",
+                "Star stuff harvesting star light",
+                "Unbounded tendrils of gossamer clouds")
 
         fun getNextPhrase(): String {
             if (counter == 9) counter = 0
