9d731df9f4c33483a5276f46642a46837a6a636c
VivekV95 <vivekv210@gmail.com>
2019-08-02 16:45:05 -0500

    Create About page layout
    Co-authored-by: JoshHalvorson <joshhalvorson16@gmail.com>
    Co-authored-by: VivekV95 <vivekv210@gmail.com>

---
+++ app/src/main/java/com/lambdaschool/empoweredconversation/fragment/AboutFragment.kt
@@ -0,0 +1,37 @@
+package com.lambdaschool.empoweredconversation.fragment
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.lambdaschool.empoweredconversation.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.toolbar_title?.text = "About"
+    }
+
+
+}
